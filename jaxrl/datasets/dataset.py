0ef0b67ba31e44b6e4fe97a3544f34b7e084647e
Anikait Singh <singh.anikait@gmail.com>
2021-07-19 10:41:06 -0700

    Initial commit

---
+++ jaxrl/datasets/dataset.py
@@ -0,0 +1,109 @@
+import collections
+
+import numpy as np
+from tqdm import tqdm
+
+Batch = collections.namedtuple(
+    'Batch',
+    ['observations', 'actions', 'rewards', 'masks', 'next_observations'])
+
+
+def split_into_trajectories(observations, actions, rewards, masks,
+                            next_observations):
+    trajs = [[]]
+
+    for i in tqdm(range(len(observations))):
+        if i > 0:
+            # Detect breaks between trajectories.
+            norm = np.linalg.norm(next_observations[i - 1] - observations[i])
+            if norm > 0.0 or masks[i - 1] == 0.0:
+                trajs.append([])
+
+        trajs[-1].append((observations[i], actions[i], rewards[i], masks[i],
+                          next_observations[i]))
+
+    return trajs
+
+
+def merge_trajectories(trajs):
+    observations = []
+    actions = []
+    rewards = []
+    masks = []
+    next_observations = []
+
+    for traj in trajs:
+        for (obs, act, rew, mask, next_obs) in traj:
+            observations.append(obs)
+            actions.append(act)
+            rewards.append(rew)
+            masks.append(mask)
+            next_observations.append(next_obs)
+
+    return np.stack(observations), np.stack(actions), np.stack(
+        rewards), np.stack(masks), np.stack(next_observations)
+
+
+class Dataset(object):
+    def __init__(self, observations: np.ndarray, actions: np.ndarray,
+                 rewards: np.ndarray, masks: np.ndarray,
+                 next_observations: np.ndarray, size: int):
+        self.observations = observations
+        self.actions = actions
+        self.rewards = rewards
+        self.masks = masks
+        self.next_observations = next_observations
+        self.size = size
+
+    def sample(self, batch_size: int) -> Batch:
+        indx = np.random.randint(self.size, size=batch_size)
+        return Batch(observations=self.observations[indx],
+                     actions=self.actions[indx],
+                     rewards=self.rewards[indx],
+                     masks=self.masks[indx],
+                     next_observations=self.next_observations[indx])
+
+    def take_top(self, percentile: float = 100.0):
+        assert percentile > 0.0 and percentile <= 100.0
+
+        trajs = split_into_trajectories(self.observations, self.actions,
+                                        self.rewards, self.masks,
+                                        self.next_observations)
+
+        def compute_returns(traj):
+            episode_return = 0
+            for _, _, rew, _, _ in traj:
+                episode_return += rew
+
+            return episode_return
+
+        trajs.sort(key=compute_returns)
+
+        N = int(len(trajs) * percentile / 100)
+        N = max(1, N)
+
+        trajs = trajs[-N:]
+
+        (self.observations, self.actions, self.rewards, self.masks,
+         self.next_observations) = merge_trajectories(trajs)
+
+        self.size = len(self.observations)
+
+    def take_random(self, percentage: float = 100.0):
+        assert percentage > 0.0 and percentage <= 100.0
+
+        trajs = split_into_trajectories(self.observations, self.actions,
+                                        self.rewards, self.masks,
+                                        self.next_observations)
+
+        np.random.shuffle(trajs)
+
+        N = int(len(trajs) * percentage / 100)
+        N = max(1, N)
+
+        trajs = trajs[-N:]
+
+        (self.observations, self.actions, self.rewards, self.masks,
+         self.next_observations) = merge_trajectories(trajs)
+
+        self.size = len(self.observations)
